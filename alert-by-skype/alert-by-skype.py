acbb9d190763cda66ec842840c719217aa427b21
root <root@hp-axs3-test.(none)>
2011-06-16 18:27:16 +0900

    - add arg option

--- alert-by-skype/alert-by-skype.py
+++ alert-by-skype/alert-by-skype.py
@@ -4,12 +4,14 @@ import time
 import wave
 import Skype4Py
 import os
+import sys
 
 ########## setting ############
 ## phone number
 #CallAddress=u'+81-XX-XXXX-XXXX'
+CallAddress=''
 ## skype id
-CallAddress=u'echo123'
+#CallAddress=u'echo123'
 Timeout=30
 SoundFile=u'/var/lib/zabbix/error.wav'
 os.environ['DISPLAY']=':0.0'
@@ -20,13 +22,21 @@ def main():
 	global CallAddress
 	global Timeout
 	global SoundFile
+	
+	if len(sys.argv) > 1:
+		CallAddress=sys.argv[1]
+	elif CallAddress != '' :
+		pass
+	else:
+		return 1
+	
 	skype = Skype4Py.Skype()
 	try:
 		call=skype.PlaceCall(CallAddress)
 	except:
 		# just skip
 		print "skype failed to make call."
-		return -1
+		return 1
 	try:
 		wavefile=wave.open(SoundFile,'r')
 		WaveLength = wavefile.getnframes() / wavefile.getframerate()
@@ -34,7 +44,7 @@ def main():
 	except:
 		# just skip
 		print "something is wrong with wave file."
-		return -1
+		return 1
 	sec = 0
 	while call.Status != Skype4Py.clsInProgress:
 		time.sleep(1)
@@ -51,7 +61,7 @@ def main():
 			if Timeout + WaveLength < sec:
 				call.Finish()
 		return 0
-	return -1
+	return 0
 
 if __name__ == "__main__":
 	main()
